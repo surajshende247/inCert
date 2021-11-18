a3354bf349da5f89cb965ca5193c4f299d65f8b1
Suraj Shende <surajshende247@gmail.com>
2021-11-18 15:29:34 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { degrees, PDFDocument, rgb, StandardFonts } from 'pdf-lib';
+import samplePDF from './blank.pdf';
 function App() {
+  async function createPdf() {
+    const url = samplePDF;
+    const existingPdfBytes = await fetch(url).then(res => res.arrayBuffer())
+  
+    const pdfDoc = await PDFDocument.load(existingPdfBytes)
+    const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica)
+  
+    const pages = pdfDoc.getPages()
+    const firstPage = pages[0]
+    const { width, height } = firstPage.getSize()
+    firstPage.drawText('This text was added with JavaScript!', {
+      x: 5,
+      y: height / 2 + 300,
+      size: 50,
+      font: helveticaFont,
+      color: rgb(0.95, 0.1, 0.1),
+      rotate: degrees(-45),
+    })
+  
+    //const pdfBytes = await pdfDoc.save()  
+
+    // Serialize the PDFDocument to bytes (a Uint8Array)
+    const pdfDataUri = await pdfDoc.saveAsBase64({ dataUri: true });
+    console.log(pdfDataUri);
+   //saveAs(pdfDataUri, "newcertificate.pdf");
+   document.getElementById("pdf").src = pdfDataUri;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={createPdf}>Create PDF</button>
+      <iframe id="pdf" width="100%" height="500px"></iframe>
     </div>
   );
 }
