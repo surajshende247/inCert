736942c04b5f02f5109d7b5513a26ecf598e9122
Suraj Shende <surajshende247@gmail.com>
2021-11-18 20:20:19 +0530

    Position for certificate number

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import { degrees, PDFDocument, rgb, StandardFonts } from 'pdf-lib';
 import samplePDF from './blank.pdf';
+
 function App() {
+
+  const certificateNo = "JULYC001";
+  const studentName = "JULIUS C. JONES";
+  const courseName = "CSCI E-101";
+  const completionDate = "01/01/2020";
+  const instructorName = "John Doe";
+
+
   async function createPdf() {
     const url = samplePDF;
     const existingPdfBytes = await fetch(url).then(res => res.arrayBuffer())
@@ -11,14 +20,22 @@ function App() {
     const pages = pdfDoc.getPages()
     const firstPage = pages[0]
     const { width, height } = firstPage.getSize()
-    firstPage.drawText('This text was added with JavaScript!', {
+    /*firstPage.drawText('This text was added with JavaScript!', {
       x: 5,
       y: height / 2 + 300,
       size: 50,
       font: helveticaFont,
       color: rgb(0.95, 0.1, 0.1),
       rotate: degrees(-45),
-    })
+    })*/
+
+    firstPage.drawText(certificateNo, {
+      x: width - 250,
+      y: height -50,
+      size: 15,
+      font: helveticaFont,
+      color: rgb(0.95, 0.1, 0.1)
+    });
   
     //const pdfBytes = await pdfDoc.save()  
 
